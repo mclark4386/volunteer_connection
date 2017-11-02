19c50378708783e2cff19647f1a9d74989c3f4e8
logic5 <johnlovejoy@Johns-MacBook.local>
2017-11-02 18:38:37 -0500

    Added category model

--- volunteerapp/models.py
+++ volunteerapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+Class Projects(models.Model):
+    category = models.CharField(max_length = 45)
+    organization = models.Foreignkey("")
