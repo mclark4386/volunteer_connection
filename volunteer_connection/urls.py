211200a3d9b4d5e051bc21f7dcc51754cdbee75b
Matthew Clark <mclark4386@gmail.com>
2017-11-03 03:43:47 -0500

    *should have a full create/login/logout pathway for users

--- volunteer_connection/urls.py
+++ volunteer_connection/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include,url
+from django.conf.urls import include, url
 from django.contrib import admin
-import volunteerapp
+# import volunteerapp
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('volunteerapp.urls')),
+    url('^', include('django.contrib.auth.urls')),
 ]
--- volunteerapp/urls.py
+++ volunteerapp/urls.py
@@ -1,10 +1,23 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from . import views
+from django.contrib.auth import views as auth_views
 
 app_name = "volunteerapp"
 
 urlpatterns = [
-    url(r'^', views.Index, name="index"),
-    url(r'^projects/$', views.Index, name="project-add"),
 
+    url(r'^$', views.Index, name="index"),
+    url(r'^projects/$', views.Index, name="project-add"),
+    url(r'^signup/$', views.UserFormView.as_view(), name="signup"),
+    url(r'^login$', auth_views.LoginView.as_view(
+        template_name='volunteerapp/login.html'), name="login"),
+    url(r'^logout$', auth_views.LogoutView.as_view(
+        next_page='volunteerapp:index'), name="logout"),
+    url(r'^password_reset$',
+        auth_views.PasswordResetView.as_view(
+            email_template_name="volunteerapp/password_reset_email.html"
+        ), name="password_reset"),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm"),
 ]
