9e30e6be25f3cd438c10a590bfbb15f0603c8646
Matthew Clark <mclark4386@gmail.com>
2017-11-07 00:46:56 -0600

    *made tag model and added it to admin panel *moved description of ProjectCategory from char field to text field

--- volunteerapp/models.py
+++ volunteerapp/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class ProjectCategory(models.Model):
     name = models.CharField(max_length=250)
-    description = models.CharField(max_length=1500)
+    description = models.TextField()
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User,
@@ -19,3 +19,12 @@ class UserProfile(models.Model):
 
     def cacheKarma(self):
         pass
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=140)
+    description = models.TextField()
+    color = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
